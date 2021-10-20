fdc74a4811e8f7d8153581d68d83aed1fa728bac
Eavan <eavan.nicholson@gmail.com>
2021-10-20 16:24:17 -0400

    commit tree f048e2e3a05ca16f524dcbdc723388f93c1729b8

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+let distance;
+
+function scuberGreetingForFeet(distance){
+  let scuberGreetingForFeet; 
+  if(distance <= '399'){
+   return 'This one is on me!' 
+  }
+  else if(distance >= '2001'){
+   return 'I will gladly take your thirty bucks.'
+  }
+  else if(distance >= '2501');{
+    return 'No can do.'
+  }
+
+}
+function ternaryCheckCity(city){
+  let ternaryCheckCity;
+  return city === 'NYC'? 'Ok, sounds good.' : 'No go.'
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case 'generous' :
+      return 'Thank you so much.'
+    case 'not as generous' :
+      return 'Thank you.'
+    case 'thanks for everything' :
+      return 'Bye.'
+  }
+   
+  
 }
